b5ad513b5ee613aeb525e22cab000dfd4cb99785
Kay Hayen <kay.hayen@gmail.com>
2023-03-19 12:23:45 +0100

    Added for first test case to cover PySide6 with QML.
    * This one is very delicate for included DLLs, so we need to
      have this, to avoid regressions such as for 1.5.1 release.

---
+++ README.rst
@@ -0,0 +1,33 @@
+Nuitka Watch
+============
+
+This repository is under construction. The intention is to detect PyPI package
+updates, and check if Nuitka is working with them.
+
+One idea is to detect ``scipy`` breaking on a platform with Nuitka due to DLL
+changes right away, and not have to wait until somebody reports it, and then
+probably not in the correct fashion.
+
+The other idea is to detect regressions inside the Nuitka plugin mechanisms
+and standalone capabilities automatically.
+
+And the important idea, is to do all of that in a distributed fashion, because
+there is no way for one user to compile everything on all platforms for every
+release of Nuitka and a PyPI package.
+
+The ``nuitka-watch`` tool will scan for installed Pythons. Right now you are
+expected to install the dependencies in this requirement files, to make it
+suitable for testing. We might enable it to do this automatically. Basically it
+pulls in the supported version of pipenv for a given Python.
+
+.. code:: bash
+
+    # For each python used:
+    python -m pip install -r requirements.txt
+
+    # Go to the base of this repository, may also go to subfolders to
+    # benefit from the structure.
+    cd Nuitka-watch
+
+    # Execute Nuitka watch. Nuitka does not have to be installed anywhere.
+    python3 ../Nuitka/bin/nuitka-watch
--- cases/PySide6/qml-example/PySideQmlPolarchartTest.py
+++ cases/PySide6/qml-example/PySideQmlPolarchartTest.py
@@ -1,19 +1,15 @@
-"""PySide2/PySide6 port of the QML Polar Chart Example from Qt v5.x"""
+"""PySide6 port of the QML Polar Chart Example from Qt v5.x"""
 
 # Decide to use either PySide2 or PySide6.
-# nuitka-project-if: "pyside2" in os.getenv("TEST_VARIANT", "standalone,pyside2,commercial").lower().split(","):
-#   nuitka-project: --enable-plugin=pyside2
-# nuitka-project-if: "pyside6" in os.getenv("TEST_VARIANT", "standalone,pyside2,commercial").lower().split(","):
-#   nuitka-project: --enable-plugin=pyside6
+# nuitka-project: --enable-plugin=pyside6
 
 # For standalone, the Qt plugins must be included, and this uses QML so add that too.
-# nuitka-project-if: "standalone" in os.getenv("TEST_VARIANT", "standalone,pyside2,commercial").split(","):
-#   nuitka-project: --standalone
-#   nuitka-project: --include-qt-plugins=sensible,qml
+# nuitka-project: --standalone
+# nuitka-project: --include-qt-plugins=sensible,qml
 
 # Commercial file inclusion vs. free file inclusion.
 # nuitka-project: --include-data-dir={MAIN_DIRECTORY}/qmlpolarchart=qmlpolarchart
-# nuitka-project-if: "commercial" in os.getenv("TEST_VARIANT", "standalone,pyside2,commercial").split(","):
+# nuitka-project-if: {Commercial} is not None:
 #   nuitka-project: --embed-data-files-qt-resource-pattern=qmlpolarchart/**
 # nuitka-project-if: {OS} == "Windows":
 #    nuitka-project: --windows-icon-from-ico={MAIN_DIRECTORY}/qmlpolarchart/Example-Icon.png
@@ -23,26 +19,14 @@
 #    nuitka-project: --macos-app-icon={MAIN_DIRECTORY}/qmlpolarchart/Example-Icon.png
 #    nuitka-project: --macos-create-app-bundle
 #    nuitka-project: --macos-sign-identity="Nuitka Test"
-#    nuitka-project-if: "pyside2" in os.getenv("TEST_VARIANT", "standalone,pyside2,commercial").split(","):
-#       nuitka-project: --onefile
-
-# Avoid options-nanny to complain, we want to see errors easily. Do not do this
-# for normal GUI deployments of course.
-# nuitka-project: --enable-console
 
 import os
 import sys
 
-try:
-    from PySide2.QtCore import QUrl
-    from PySide2.QtGui import QIcon
-    from PySide2.QtQuick import QQuickView
-    from PySide2.QtWidgets import QApplication
-except ImportError:
-    from PySide6.QtCore import QUrl
-    from PySide6.QtGui import QIcon
-    from PySide6.QtQuick import QQuickView
-    from PySide6.QtWidgets import QApplication
+from PySide6.QtCore import QUrl
+from PySide6.QtGui import QIcon
+from PySide6.QtQuick import QQuickView
+from PySide6.QtWidgets import QApplication
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
