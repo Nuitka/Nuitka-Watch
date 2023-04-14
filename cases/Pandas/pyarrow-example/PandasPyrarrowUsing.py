4a9194900a60bd0f1bd83b6761fe82987354ee6e
Kay Hayen <kay.hayen@gmail.com>
2023-04-14 10:28:50 +0200

    Added pandas+pyarrow using test case

---
+++ cases/Pandas/pyarrow-example/PandasPyrarrowUsing.py
@@ -0,0 +1,16 @@
+# nuitka-project: --standalone
+# nuitka-project: --noinclude-default-mode=error
+# nuitka-project: --noinclude-custom-mode=numpy.distutils:error
+
+import os
+
+import pandas
+import pandas._libs.tslibs.base
+import pandas._libs.window
+from pandas.io.formats.style import Styler
+
+df = pandas.DataFrame({"a": [1, 2], "b": [3, 4]})
+df.to_feather("test-data.feather")
+print(pandas.read_feather("test-data.feather"))
+os.unlink("test-data.feather")
+print("OK")
