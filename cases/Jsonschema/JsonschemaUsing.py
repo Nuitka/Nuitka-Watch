612c0f8ee6fa8413a694890647a7a841ea3ed142
Kay Hayen <kay.hayen@gmail.com>
2023-07-07 10:03:25 +0200

    Adding test case for jsonschema which frequently changes

---
+++ cases/Jsonschema/JsonschemaUsing.py
@@ -0,0 +1,30 @@
+# nuitka-project: --standalone
+
+from jsonschema import ValidationError, validate
+
+if __name__ == '__main__':
+    schema = {
+        "type": "object",
+        "properties": {
+            "price": {
+                "type": "number"
+            },
+            "name": {
+                "type": "string"
+            },
+        },
+    }
+
+    validate(instance={"name": "Eggs", "price": 34.99}, schema=schema)
+    try:
+        validate(
+            instance={
+                "name": "Eggs",
+                "price": "Invalid"
+            },
+            schema=schema,
+        )
+    except ValidationError as e:
+        print(e.message)
+        print(e.absolute_path)
+        print(e.absolute_schema_path)
