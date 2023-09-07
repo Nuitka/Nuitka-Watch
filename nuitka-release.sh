5e1d488ba51204d9b8513b84b2bad2654a0fa4ed
Kay Hayen <kay.hayen@gmail.com>
2023-09-07 07:08:31 -0700

    Added scripts for common tasks

---
+++ nuitka-release.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+exec python3.10 ../Py2C/bin/nuitka-watch --python-version=3.10 --no-pipenv-update --nuitka-binary=../Nuitka-develop/bin/nuitka
---
+++ pypi-update.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+exec python3.10 ../Py2C/bin/nuitka-watch --python-version=3.10 --nuitka-binary=../Nuitka-develop/bin/nuitka
