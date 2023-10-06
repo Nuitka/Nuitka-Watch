22d5e25bee70740e75d1299cedbdc8c95b89c7ea
Kay Hayen <kay.hayen@gmail.com>
2023-10-06 11:38:58 +0200

    Added results for MSYS2 Cairo test case

--- cases/Gtk/Cairo/CairoTest.py
+++ cases/Gtk/Cairo/CairoTest.py
@@ -1,14 +1,17 @@
 # nuitka-project: --standalone
+# nuitka-project: --include-data-file={MAIN_DIRECTORY}/Example-Icon.png=Example-Icon.png
+
+import os
 
 import cairo
 import gi
-from gi.repository import Gdk, Gtk
-
 gi.require_version("Gtk", "3.0")
 gi.require_version("Gdk", "3.0")
+from gi.repository import Gdk, Gtk
+
 Gtk.init()
 
-image = cairo.ImageSurface.create_from_png("doc/images/Nuitka-Logo-Symbol.png")
+image = cairo.ImageSurface.create_from_png(os.path.join(os.path.dirname(__file__), "Example-Icon.png"))
 region = Gdk.cairo_region_create_from_surface(image)
 
 print("All OK")
