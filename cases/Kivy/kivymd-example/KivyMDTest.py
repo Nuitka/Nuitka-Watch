1a57e0f81e6f209eb662b54aad3c37bd3048dee1
Kay Hayen <kay.hayen@gmail.com>
2023-10-25 12:39:56 +0200

    Added KivyMD example, for use starting with 1.9 release
    * In 1.8 data files would be missing, and we expect no hotfix
      anymore to change that.

---
+++ cases/Kivy/kivymd-example/KivyMDTest.py
@@ -0,0 +1,21 @@
+# nuitka-project: --standalone
+
+from kivy.core.window import Window
+from kivy.lang import Builder
+from kivymd.app import MDApp
+
+Window.size = (360, 640)
+
+kv = """
+MDRaisedButton:
+    text: "Button"
+    pos_hint: {"center_x":0.5,"center_y":0.5}
+"""
+
+
+class kivy(MDApp):
+    def build(self):
+        return Builder.load_string(kv)
+
+
+kivy().run()
