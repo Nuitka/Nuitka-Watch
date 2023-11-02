33ab8e4208c895d1dca08ee5bf7aa545a5980809
Kay Hayen <kay.hayen@gmail.com>
2023-11-02 11:40:10 +0100

    WIP: Added symlink data files test case
    * No case config yet, because this is incomplete, but it's been
      in my checkout for too long now.

---
+++ cases/Symlinks/SymlinksMain.py
@@ -0,0 +1,17 @@
+# nuitka-project: --standalone
+
+# nuitka-project: --include-data-file={MAIN_DIRECTORY}/link_target=link_target
+# nuitka-project: --include-data-file={MAIN_DIRECTORY}/link_source=link_source
+# nuitka-project: --include-data-file={MAIN_DIRECTORY}/subdir/link_source2=subdir/link_source2
+
+import os
+
+with open(os.path.join(os.path.dirname(__file__), "link_source")) as data_file:
+    print(data_file.read())
+
+with open(
+    os.path.join(os.path.dirname(__file__), "subdir", "link_source2")
+) as data_file:
+    print(data_file.read())
+
+print("OK")
