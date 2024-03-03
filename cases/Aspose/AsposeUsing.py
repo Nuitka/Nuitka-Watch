e07bb535bef421231d93f0d799c699ef2f4fa220
Kay Hayen <kay.hayen@gmail.com>
2024-03-03 10:22:07 +0100

    Added test case for Aspose
    * Need to see if this works on non-Win32 at all
      and needs more tweaks, for 2.1 release.

---
+++ cases/Aspose/AsposeUsing.py
@@ -0,0 +1,15 @@
+# nuitka-project: --standalone
+# nuitka-project: --include-data-file={MAIN_DIRECTORY}/testing.eml=testing.eml
+
+import os
+
+from aspose.email import mapi
+
+def main():
+    msg = mapi.MapiMessage.load(os.path.join(os.path.dirname(__file__), r"testing.eml"))
+    print("Subject:", msg.subject)
+    print("From:", msg.sender_email_address)
+
+
+if __name__ == "__main__":
+    main()
