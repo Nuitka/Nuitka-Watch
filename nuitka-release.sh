1163446ca246b0d8a432295147e7f9e5068a4a11
Kay Hayen <kay.hayen@gmail.com>
2024-03-17 19:07:03 +0100

    Fix, we moved away from my internal repo name but this script still has it

--- nuitka-release.sh
+++ nuitka-release.sh
@@ -10,10 +10,18 @@ then
     alias python3='/c/Python310_64/python.exe'
 fi
 
+# This enables my private development slightly better.
+if [ -d "../Py2C" ]
+then
+    NUITKA_WATCH="../Py2C"
+else
+    NUITKA_WATCH="../Nuitka-factory"
+fi
+
 if [ "$OS" = "Darwin" ]
 then
-    python3 ../Py2C/bin/nuitka-watch --python-version=3.10 --no-pipenv-update --nuitka-binary=../Nuitka-develop/bin/nuitka $@
-    python3 ../Py2C/bin/nuitka-watch --python-version=3.11 --no-pipenv-update --nuitka-binary=../Nuitka-develop/bin/nuitka $@
+    python3 $NUITKA_WATCH/bin/nuitka-watch --python-version=3.10 --no-pipenv-update --nuitka-binary=../Nuitka-develop/bin/nuitka $@
+    python3 $NUITKA_WATCH/bin/nuitka-watch --python-version=3.11 --no-pipenv-update --nuitka-binary=../Nuitka-develop/bin/nuitka $@
 else
-    python3 ../Py2C/bin/nuitka-watch --python-version=3.10 --no-pipenv-update --nuitka-binary=../Nuitka-develop/bin/nuitka $@
+    python3 $NUITKA_WATCH/bin/nuitka-watch --python-version=3.10 --no-pipenv-update --nuitka-binary=../Nuitka-develop/bin/nuitka $@
 fi
