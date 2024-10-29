58bd6dccbf75125a27e9e392a8d8e44e321a1c30
Kay Hayen <kay.hayen@gmail.com>
2024-10-29 03:37:09 -0700

    Cleanup test case

--- cases/PyGame/opengl-example/PyGameOpenGLTest.py
+++ cases/PyGame/opengl-example/PyGameOpenGLTest.py
@@ -1,10 +1,11 @@
 # nuitka-project: --standalone
 
-import pygame
-from pygame.locals import *
+import sys
 
+import pygame
 from OpenGL.GL import *
 from OpenGL.GLU import *
+from pygame.locals import *
 
 verticies = (
     (1, -1, -1),
@@ -14,23 +15,23 @@ verticies = (
     (1, -1, 1),
     (1, 1, 1),
     (-1, -1, 1),
-    (-1, 1, 1)
-    )
+    (-1, 1, 1),
+)
 
 edges = (
-    (0,1),
-    (0,3),
-    (0,4),
-    (2,1),
-    (2,3),
-    (2,7),
-    (6,3),
-    (6,4),
-    (6,7),
-    (5,1),
-    (5,4),
-    (5,7)
-    )
+    (0, 1),
+    (0, 3),
+    (0, 4),
+    (2, 1),
+    (2, 3),
+    (2, 7),
+    (6, 3),
+    (6, 4),
+    (6, 7),
+    (5, 1),
+    (5, 4),
+    (5, 7),
+)
 
 
 def Cube():
@@ -43,21 +44,21 @@ def Cube():
 
 def main():
     pygame.init()
-    display = (800,600)
-    pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
+    display = (800, 600)
+    pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
 
-    gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
+    gluPerspective(45, (display[0] / display[1]), 0.1, 50.0)
 
-    glTranslatef(0.0,0.0, -5)
+    glTranslatef(0.0, 0.0, -5)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
-                quit()
+                sys.exit(0)
 
         glRotatef(1, 3, 1, 1)
-        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT)
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
         Cube()
         pygame.display.flip()
         pygame.time.wait(10)
