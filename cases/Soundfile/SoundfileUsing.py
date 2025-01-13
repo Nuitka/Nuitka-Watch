538e199b02d4ebe6e5e0e7262bcb4daf1d229a69
Kay Hayen <kay.hayen@gmail.com>
2025-01-13 09:57:15 +0100

    Added Windows results for older soundfile

--- cases/Soundfile/SoundfileUsing.py
+++ cases/Soundfile/SoundfileUsing.py
@@ -1,6 +1,8 @@
 # nuitka-project: --standalone
 # nuitka-project: --include-data-file={MAIN_DIRECTORY}/test-44100Hz-be-1ch-4bytes.wav=test-44100Hz-be-1ch-4bytes.wav
 
+from __future__ import print_function
+
 import os
 
 import soundfile as sf
@@ -8,3 +10,5 @@ import numpy as np
 
 rms = [np.sqrt(np.mean(block**2)) for block in
        sf.blocks(os.path.join(os.path.dirname(__file__), 'test-44100Hz-be-1ch-4bytes.wav'), blocksize=1024, overlap=512)]
+
+print(rms)
