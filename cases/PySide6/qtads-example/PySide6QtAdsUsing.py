c8a353fd994f2cc87c696feaa351d35980b70ad5
Kay Hayen <kay.hayen@gmail.com>
2025-08-19 09:19:11 +0200

    Added coverage for sponsor's package

---
+++ cases/PySide6/qtads-example/PySide6QtAdsUsing.py
@@ -0,0 +1,8 @@
+# nuitka-project: --mode=standalone
+# nuitka-project: --enable-plugin=pyside6
+
+import PySide6QtAds
+print(PySide6QtAds)
+print(PySide6QtAds.CDockWidget)
+
+from PySide6QtAds import CDockWidget
